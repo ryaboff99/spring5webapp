d8ee87d5f80f9d22eef8943563afaba1e1d27463
Oleksii Riabov <ryaboff99@gmail.com>
2022-06-03 13:34:12 +0300

    Create "Book" POJO class as an Entity
    * Define id variable
    * Define firstName variable
    * Define lastName variable
    * Define books HashSet
    * Define no args constructor
    * Define all args constructor
    * Define getters and setters
    * Define equals method
    * Define hashCode method
    * Define toString method
    * Annotate Book class with @Entity annotation
    * Annotate id value with @Id and @GeneratedValue annotations
    * Define "Many to Many" relationship between "Authors" and "Book" classes

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -0,0 +1,87 @@
+package guru.springframework.spring5webapp.domain;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity // tells Hibernate that this is an entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // automatic ID generation by Database
+    private Long id; // Primary Key
+
+    private String title;
+    private String isbn;
+
+    // Author's Set will have such "Many To Many" implementation: @ManyToMany(mappedBy = "authors") private Set<Book> books; - mapped by variable "authors"
+    @ManyToMany // Database will have "author" table and "book" table and they will be joined in table "author_book" that will hold relationship between "author" and "book" tables
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), // @JoinColumn setting up the properties within the JoinTable
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors = new HashSet<>();
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn) {
+        this.title = title;
+        this.isbn = isbn;
+    }
+
+    public Long getId() {   // getter for Primary Key
+        return id;
+    }
+
+    public void setId(Long id) {    // setter for Primary Key
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id != null ? id.equals(book.id) : book.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
