4492c7894e24b8626f732ce5c6f322a1392ccaa3
Oleksii Riabov <ryaboff99@gmail.com>
2022-06-03 13:35:07 +0300

    Create "Author" POJO class as an Entity
    * Define id variable
    * Define firstName variable
    * Define lastName variable
    * Define books HashSet
    * Define no args constructor
    * Define all args constructor
    * Define getters and setters
    * Define equals method
    * Define hashCode method
    * Define toString method
    * Annotate Book class with @Entity annotation
    * Annotate id value with @Id and @GeneratedValue annotations
    * Define "Many to Many" relationship between "Authors" and "Book" classes

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -0,0 +1,84 @@
+package guru.springframework.spring5webapp.domain;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity // tells Hibernate that this is an entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // automatic ID generation by Database
+    private Long id; // Primary Key
+
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<>();
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {   // getter for Primary Key
+        return id;
+    }
+
+    public void setId(Long id) { // setter for Primary Key
+        this.id = id;
+    }
+
+    public String getFirstName() {  // common getters and setters
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
